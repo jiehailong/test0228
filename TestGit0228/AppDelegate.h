3a2c357b81e00b2ac85fcd573340b70360fb39ec
jiehailong <316042236@qq.com>
2018-02-28 22:46:20 +0800

    xxxx

---
+++ TestGit0228/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit0228
+//
+//  Created by salonjack on 2018/2/28.
+//  Copyright © 2018年 hljie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit0228/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit0228
+//
+//  Created by salonjack on 2018/2/28.
+//  Copyright © 2018年 hljie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
